f7303952581bd653639ebfecc7be10089f4c5aad
scriiipt <kilo-don2013@yandex.ru>
2022-04-28 17:20:21 +0300

    main

--- insides/admin.py
+++ insides/admin.py
@@ -10,17 +10,15 @@ admin.site.register(Books, BooksAdmin)
 
 @admin.register(Authors)
 class AuthorsAdmin(admin.ModelAdmin):
-  list_display =['name', 'id']
+  list_display =['name', 'id', 'books_count']
   
-   
-"""     def books_count(self, obj):
-        return obj.books_count
+  def books_count(self, obj):
+          return obj.books_count
 
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        queryset = queryset.annotate(books__count=Count("name"))
-        return queryset """
-    
+  def get_queryset(self, request):
+          queryset = super().get_queryset(request)
+          queryset = queryset.annotate(books_count=Count("books"))
+          return queryset
 
 
 
